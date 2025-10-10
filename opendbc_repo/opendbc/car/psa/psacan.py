e6fef8bc84f4af53bf18e950da06ee555be01048
cristian zantedeschi <c.zantedeschi@gmail.com>
2025-10-10 10:47:22 +0200

    Puntato il submodule opendbc a cristianku/opendbc (branch: peugeot-3008)

--- opendbc_repo/opendbc/car/psa/psacan.py
+++ opendbc_repo/opendbc/car/psa/psacan.py
@@ -1,3 +1,4 @@
+//asdfadsfasd
 def psa_checksum(address: int, sig, d: bytearray) -> int:
   chk_ini = {0x452: 0x4, 0x38D: 0x7, 0x42D: 0xC}.get(address, 0xB)
   byte = sig.start_bit // 8
@@ -6,13 +7,16 @@ def psa_checksum(address: int, sig, d: bytearray) -> int:
   return (chk_ini - checksum) & 0xF
 
 
-def create_lka_steering(packer, lat_active: bool, apply_angle: float, status: int):
+def create_lka_steering(packer, lat_active: bool, apply_torque: float, status: int):
   values = {
-    'DRIVE': 1,
+    'TORQUE': apply_torque,
+    # 'LANE_DEPARTURE':0 if not lat_active else 1 if torque>0 else 2,
+    # 'DRIVE': 1,
     'STATUS': status,
-    'LXA_ACTIVATION': 1,
+    # 'LXA_ACTIVATION': 1,
     'TORQUE_FACTOR': lat_active * 100,
-    'SET_ANGLE': apply_angle,
+    'SET_ANGLE': 0,
   }
 
   return packer.make_can_msg('LANE_KEEP_ASSIST', 0, values)
+// ciao sdfads 
