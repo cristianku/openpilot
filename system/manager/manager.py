7f2aec150e843d74eb75995b94e0996c66d1988d
cristian zantedeschi <c.zantedeschi@gmail.com>
2025-10-28 16:38:38 +0100

    c

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -88,6 +88,8 @@ def manager_init() -> None:
                        dirty=build_metadata.openpilot.is_dirty,
                        device=HARDWARE.get_device_type())
 
+  params.put_bool("ExperimentalMode", True)
+
   # preimport all processes
   for p in managed_processes.values():
     p.prepare()
